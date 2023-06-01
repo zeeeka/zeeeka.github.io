726cd5088c73b123a60df8bf1caefd6982a013a4
zeeeka <93159922+zeeeka@users.noreply.github.com>
2023-06-02 00:21:07 +0200

    abcdefg

---
+++ portfolio/script.js
@@ -0,0 +1,37 @@
+let bars = document.getElementsByClassName ('bars')[0];
+let sideList = document.getElementsByClassName('sidelist')[0];
+let close = document.getElementsByClassName('closemenu')[0] ;
+let transLayer = document.getElementsByClassName('transparent')[0];
+bars.onclick = function () {
+    sideList.style.display='block';
+    bars.style.display='none';
+    transLayer.style.display='block';
+}
+close.onclick = function (){
+    sideList.style.display='none';
+    transLayer.style.display = "none" ;
+    if (window.matchMedia('(max-width : 540px)').matches){
+        bars.style.display='flex';
+    }else{
+        bars.style.display='none';
+    }
+}
+setInterval(function(){
+    if (!window.matchMedia('(max-width : 540px)').matches ){
+        bars.style.display='none';
+    }else{
+        bars.style.display='flex';
+    }
+} , 1000)
+
+
+/*longtext*/
+let longText = document.getElementsByClassName('longtext')[0]
+let media = window.matchMedia('(max-width : 980px)')
+setInterval(() => {
+    if(media.matches){
+        longText.innerHTML = "I’m a Front-end developer and web designer"
+    }else {
+        longText.innerHTML = "I’m a Front-end developer<br>and web designer"
+    }
+} , 1000);
