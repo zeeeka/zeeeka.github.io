5b1621923744ba6fb5fde9460c0c022404116d14
zeeeka <93159922+zeeeka@users.noreply.github.com>
2023-06-02 08:39:09 +0200

    new edit

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,14 +2,28 @@ let bars = document.getElementsByClassName ('bars')[0];
 let sideList = document.getElementsByClassName('sidelist')[0];
 let close = document.getElementsByClassName('closemenu')[0] ;
 let transLayer = document.getElementsByClassName('transparent')[0];
+let body = document.getElementById('body')
 bars.onclick = function () {
     sideList.style.display='block';
     bars.style.display='none';
     transLayer.style.display='block';
+    body.style.overflow = 'hidden' ;
+    console.log(body)
 }
 close.onclick = function (){
     sideList.style.display='none';
     transLayer.style.display = "none" ;
+    body.style.overflow='visible';
+    if (window.matchMedia('(max-width : 540px)').matches){
+        bars.style.display='flex';
+    }else{
+        bars.style.display='none';
+    }
+}
+transLayer.onclick = function (){
+    sideList.style.display='none';
+    transLayer.style.display = "none" ;
+    body.style.overflow='visible';
     if (window.matchMedia('(max-width : 540px)').matches){
         bars.style.display='flex';
     }else{
